98cff06754c73ef6fa563c170fef449024bad24a
Chris <you@example.com>
2013-12-11 07:14:18 +0000

    example csv uploader to blobstore updated

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import urllib
 import webapp2
 
+import csv
 from google.appengine.ext import blobstore
 from google.appengine.ext.webapp import blobstore_handlers
 
@@ -23,7 +24,8 @@ class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
   def get(self, resource):
     resource = str(urllib.unquote(resource))
     blob_info = blobstore.BlobInfo.get(resource)
-    self.send_blob(blob_info)
+    self.response.write("Going to write blob")
+    #self.send_blob(blob_info)
 
 app = webapp2.WSGIApplication([('/', MainHandler),
                                ('/upload', UploadHandler),
