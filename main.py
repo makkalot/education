c1b9a7a934134e3eab17946d4031550cf1a971ea
ray <test@Rays-MacBook-Pro.local>
2014-01-04 20:19:16 -0500

    add encryption

--- main.py
+++ main.py
@@ -1,29 +1,59 @@
-import os
+from __future__ import with_statement
 import urllib
 import webapp2
 
 from google.appengine.ext import blobstore
 from google.appengine.ext.webapp import blobstore_handlers
+from google.appengine.api import files
+from Crypto.PublicKey import RSA
+from Crypto import Random
 
 class MainHandler(webapp2.RequestHandler):
-  def get(self):
-    upload_url = blobstore.create_upload_url('/upload')
-    self.response.out.write('<html><body>')
-    self.response.out.write('<form action="%s" method="POST" enctype="multipart/form-data">' % upload_url)
-    self.response.out.write("""Upload File: <input type="file" name="file"><br> <input type="submit"
-        name="submit" value="Submit"> </form></body></html>""")
+    def get(self):
+        self.response.out.write('''<html><body><form action="/upload" enctype="multipart/form-data" method="post"><input type="file" name="file"/><input type="submit" /></form><body/><html/>''')
 
-class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
-  def post(self):
-    upload_files = self.get_uploads('file')  # 'file' is file upload field in the form
-    blob_info = upload_files[0]
-    self.redirect('/serve/%s' % blob_info.key())
+class UploadHandler(webapp2.RequestHandler):
+    def crypt(self, plaintext):
+        # Use a fixed private key here to get a deterministic result for testing
+        key = RSA.importKey('''-----BEGIN RSA PRIVATE KEY-----
+        MIICXAIBAAKBgQCIJAdEmiawxYaf/ZTv/679mwxiwJPhHsKPIfoW8w0IRy0oZhkS
+        zp+M+UGIiKL3FDJjMkVVl8mpxJ8qMwkTkRte8+1GoxPRANmuvEsAIVpfVctJkIqJ
+        +FTcH8J28hPugIJFrWD4tWcPslr75s8fx0VJjcOkdV5gZAea2JlXKaXEvQIDAQAB
+        AoGAOkVJgxiD5Pe2xrYQUKVcrhn2NDJ/WUUEO6VsWPRRKLDmaDtDEiS0b++kGB97
+        uUvAwWqb+KXOYEbTZYmQofpi/yKSzDIgJy04u2LSmyAvlWrJzj3GE6NbHPv/ctlY
+        YUD51TFn3cc97TYCH+fW7HPxpbrRDr9sHvIC7f3vV5HuFJUCQQC6n23oUH3xQeIb
+        oNFLQ/GAIQULYQOEYSJv7Vy3nzkkNAKQxDP9/OcYgsGcK3VxzHVrjQVOjFX/kM1L
+        d+OtqjtbAkEAusBPWAqUT2CGenG11Vwz5dy1RP4k+xYjI7RLh7dsxGrAshl8YYVx
+        ILMvYtNNOFlo9cVCc7zRmUKu175j7kOzxwJBALX8pKwoWjh7W+g/UfnInueoy4eG
+        KmzcYD2vxXuWvJ1OTrYnbuAe0Kj5UZ5eTuATVunzkhpABdj7twcCObdvywMCQEHH
+        cysjrtG2widm3hFlBLK2ZvMCQaxfQ8lTvDb1mM4me/E/oNwI0Kwf8VTx8IUkmR/Y
+        d2uk2n8NSeCcIz7NggkCQFylnC7S7hlgZyUvaIpBsGzzdr8cyrTol4T1G9n7G2sE
+        Q1pc7/sXaYlMBZxKrCMWjAol9AZ2Cfpj6x5A3XnTm98=
+        -----END RSA PRIVATE KEY-----''')
+        public_key = key.publickey()
+        enc_data = public_key.encrypt(plaintext, 32)
+        
+        # encode the byte data into ASCII data so that it could be printed out in the browser
+        return enc_data[0].encode('base64')
 
+    def post(self):
+        rows=self.request.POST.get('file').value.split("\n")
+        file_name = files.blobstore.create(mime_type='text/plain')
+        with files.open(file_name, 'a') as f:
+            for row in rows:
+                items = row.split(",")
+                if len(items) > 1:
+                    items[1] = self.crypt(items[1])
+                f.write("%s\n" % (",".join(items)))
+        files.finalize(file_name)
+        blob_key = files.blobstore.get_blob_key(file_name)
+        self.response.out.write('<html><body>File has uploaded, please download the file at <a href="/serve/%s">here</a>' % blob_key )
+    
 class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
-  def get(self, resource):
-    resource = str(urllib.unquote(resource))
-    blob_info = blobstore.BlobInfo.get(resource)
-    self.send_blob(blob_info)
+    def get(self, resource):
+        resource = str(urllib.unquote(resource))
+        blob_info = blobstore.BlobInfo.get(resource)
+        self.send_blob(blob_info)
 
 app = webapp2.WSGIApplication([('/', MainHandler),
                                ('/upload', UploadHandler),
